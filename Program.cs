0dc60467f3c9a4961d403853bf22826ccec7dab4
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-05 15:41:48 +0300

    Added game initializer

--- Program.cs
+++ Program.cs
@@ -11,18 +11,17 @@ namespace SolitaryDungeon
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
-
+            Game.Initialize();
             Level lvl1 = new Level(20, 20);
             Game.CurentLevel = lvl1;
             Player p = new Player(lvl1, 3, 3);
             while (true)
             {
                 Thread.Sleep(16);
-                lvl1.Update();
                 p.Update();
+                //Camera.Render();
             }
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
