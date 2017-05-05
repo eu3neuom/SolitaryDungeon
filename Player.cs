44c7e50618a97a945fe7dec27fe7f33231113cef
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-05 11:08:56 +0300

    Basic player

---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaryDungeon
+{
+    class Player : Character
+    {
+        public Player(int Xposition, int Yposition) : base(Xposition, Yposition, ConsoleColor.White) { }
+
+        protected override void ExecuteBehaviour()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                switch (cki.Key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        Sprite = '▲';
+                        Move(Direction.Up);
+                        break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        Sprite = '▼';
+                        Move(Direction.Down);
+                        break;
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow:
+                        Sprite = '◄';
+                        Move(Direction.Left);
+                        break;
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        Sprite = '►';
+                        Move(Direction.Right);
+                        break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
