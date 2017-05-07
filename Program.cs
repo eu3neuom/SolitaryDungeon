63b7754a923eb631ce2a77f1c0426eb032ec5904
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-07 23:26:23 +0300

    Tested some stuff

--- Program.cs
+++ Program.cs
@@ -12,9 +12,10 @@ namespace SolitaryDungeon
         static void Main(string[] args)
         {
             Game.Initialize();
-            Level lvl1 = new Level(20, 20);
+            Level lvl1 = new Level(50, 50);
             Game.CurentLevel = lvl1;
             Player p = new Player(lvl1, 3, 3);
+
             while (true)
             {
                 Thread.Sleep(16);
