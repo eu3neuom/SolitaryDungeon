dfd3398c7212195237377d898212ad5bf53af64e
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-07 23:23:30 +0300

    Implemented door

---
+++ Door.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaryDungeon
+{
+    class Door : Tile
+    {
+        public Door(char Sprite, bool IsOpen) : base(Sprite, ConsoleColor.Gray, !IsOpen)
+        {
+            auxSprite = Sprite;
+            if (IsOpen)
+                this.Sprite = ' ';
+            else
+                this.Sprite = auxSprite;
+        }
+
+        public override void ExecuteBehaviour()
+        {
+            if (IsSolid)
+            {
+                IsSolid = false;
+                Sprite = ' ';
+                InGameMenu.Log("You opened a door.");
+            }
+            else
+            {
+                IsSolid = true;
+                Sprite = auxSprite;
+                InGameMenu.Log("You closed a door.");
+            }
+        }
+
+        public struct Sprites
+        {
+            public const char Horizontal = '─';
+            public const char Vertical = '│';
+        }
+
+        private char auxSprite;
+    }
+}
