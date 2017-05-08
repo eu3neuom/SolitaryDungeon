bc294ce1325fb0db41a0d315bd6527fb95bd3c98
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-08 11:48:43 +0300

    Added Zombie from build 2 now with good pathfinding

---
+++ Zombie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaryDungeon
+{
+    class Zombie : Character
+    {
+        public Zombie(Level Level, int Xposition, int Yposition) : base(Level, Xposition, Yposition, ConsoleColor.Green)
+        {
+            Sprite = 'Z';
+        }
+
+        protected override void ExecuteBehaviour()
+        {
+            Player p = Level.Player;
+            Move(Pathfinding.NextStep(Xpos, Ypos, p.Xpos, p.Ypos));
+        }
+    }
+}
