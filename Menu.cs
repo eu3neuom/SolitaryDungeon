79d3b789eb8fd88c25138b463a3efc90dd311318
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-09 11:22:42 +0300

    Added basic menu

---
+++ Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaryDungeon
+{
+    class Menu
+    {
+        public static void Show()
+        {
+            DrawMenu();
+        }
+
+        private static void DrawMenu()
+        {
+            Console.SetCursorPosition(4, 3);
+            Console.Write('╒');
+            for (int i = 0; i < Console.WindowWidth - 10; ++i)
+                Console.Write('─');
+            Console.Write('╕');
+            for (int i = 4; i < Console.WindowHeight - 4; ++i)
+            {
+                Console.SetCursorPosition(4, i);
+                Console.Write('│');
+                for (int j = 0; j < Console.WindowWidth - 10; ++j)
+                    Console.Write(' ');
+                Console.Write('│');
+            }
+            Console.SetCursorPosition(4, Console.WindowHeight - 4);
+            Console.Write('╘');
+            for (int i = 0; i < Console.WindowWidth - 10; ++i)
+                Console.Write('─');
+            Console.Write('╛');
+        }
+    }
+}
