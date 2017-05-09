7775f5ec990384715256b2835d7bf7a6e5263765
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2017-05-09 10:47:09 +0300

    Finally added true game loop

--- Program.cs
+++ Program.cs
@@ -12,16 +12,19 @@ namespace SolitaryDungeon
         static void Main(string[] args)
         {
             Game.Initialize();
-            Level lvl1 = new Level(50, 50);
+            Level lvl1 = new Level(10, 10);
             Game.CurentLevel = lvl1;
             Player p = new Player(lvl1, 3, 3);
             Zombie z = new Zombie(lvl1, 8, 8);
 
-            while (true)
+            while (Game.IsAlive)
             {
                 Thread.Sleep(16);
                 p.Update();
             }
+            Console.SetCursorPosition(0, 0);
+            Console.Write("!TZEAPA!");
+            Console.ReadKey();
         }
     }
 }
